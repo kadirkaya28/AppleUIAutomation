bc155d277f2dabc5075b6860c648c7118af2e01c
Burak Bayramoğlu <bbayramoglu99@hotmail.com>
2022-04-18 17:12:58 +0300

    Add files via upload

--- tests/test_buy_iphone.py
+++ tests/test_buy_iphone.py
@@ -21,53 +21,23 @@ class Test(BaseTest):
         rev_bag_page = ReviewBagPage(self.driver)
 
         assert self.driver.title == assertion_values["main_title"]
-        main_page.wait(main_page.IPHONE, assertion_values["menu_text"])
         assert main_page.get_element_text(main_page.IPHONE) == assertion_values["menu_text"]
-
-        main_page.highlight(main_page.IPHONE, "red")
-        main_page.click_visibility(main_page.IPHONE)
+        main_page.click_to_iphone()
 
         assert self.driver.title == assertion_values["iphone_title"]
         assert iphone_page.get_element_text(iphone_page.XIII_PRO) == assertion_values["menu_iphone_text"]
-
-        iphone_page.highlight(iphone_page.XIII_PRO, "red")
-        iphone_page.click_visibility(iphone_page.XIII_PRO)
+        iphone_page.select_xii_pro()
 
         assert self.driver.title == assertion_values["pro_title"]
         assert pro_page.get_element_text(pro_page.BUY_BUTTON) == assertion_values["buy_button"]
         assert pro_page.is_clickable(pro_page.BUY_BUTTON)
+        pro_page.click_purchase_button()
 
-        pro_page.highlight(pro_page.BUY_BUTTON, "red")
-        pro_page.click_visibility(pro_page.BUY_BUTTON)
+        conf_page.configure_iphone_specs()
 
-        max_phone = conf_page.find_elements(conf_page.PRO_OR_MAX)[1]
-        conf_page.click_presence(max_phone)
+        conf_page.configure_additional_options(country)
 
-        conf_page.click_presence(conf_page.COLORS)
+        conf_page.add_and_go_to_bag()
 
-        capacity = conf_page.find_elements(conf_page.CAPACITY)[3]
-        conf_page.click_presence(capacity)
-
-        if country != "Turkey":
-            if country != "United Kingdom" and country != "Deutschland" and country != "Italy" and country != "France" and country != "Spain":
-                carrier = conf_page.find_elements(conf_page.CARRIERS)[4]
-                conf_page.click_presence(carrier)
-
-            conf_page.click_presence(conf_page.NO_TRADE)
-
-            if country != "France":
-                payment_option = conf_page.find_elements(conf_page.PAYMENT_OPTIONS)[1]
-                conf_page.click_presence(payment_option)
-
-            conf_page.click_presence(conf_page.APPLE_CARE)
-
-        conf_page.highlight(conf_page.ADD_BUTTON, "red")
-        conf_page.click_presence(conf_page.ADD_BUTTON)
-
-        conf_page.highlight(conf_page.REVIEW_BAG, "red")
-        conf_page.click_presence(conf_page.REVIEW_BAG)
-
-        rev_bag_page.highlight(rev_bag_page.REMOVE_BUTTON, "red")
-        rev_bag_page.click_presence(rev_bag_page.REMOVE_BUTTON)
-
+        rev_bag_page.remove_iphone()
         assert assertion_values["page_source"] in self.driver.page_source
