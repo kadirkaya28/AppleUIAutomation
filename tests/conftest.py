0e053b364dda754bda31e1af485dc16ac47c7f91
Burak Bayramoğlu <bbayramoglu99@hotmail.com>
2022-04-21 17:07:37 +0300

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,23 @@
-import pytest
-from selenium.webdriver import Chrome
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver import Chrome, Firefox
+from config.Config import TestData
+import pytest
 
 
-@pytest.fixture(params=["chrome"], scope="class")
+@pytest.fixture(params=TestData.BROWSERS, scope="class")
 def init_driver(request):
-    service = ChromeService(ChromeDriverManager(log_level=0).install())
-    driver = Chrome(service=service)
+    if request.param == "chrome":
+        service = ChromeService(ChromeDriverManager(log_level=0).install())
+        driver = Chrome(service=service)
+    elif request.param == "firefox":
+        service = FirefoxService(GeckoDriverManager(log_level=0).install())
+        driver = Firefox(service=service)
+    else:
+        driver = None
+        raise AttributeError("There is no driver such exists.")
     driver.maximize_window()
     request.cls.driver = driver
     yield
--- tests/test_buy_iphone.py
+++ tests/test_buy_iphone.py
@@ -5,7 +5,7 @@ from pages.ConfigurationPage import ConfigurationPage
 from pages.IphonePage import IphonePage
 from pages.MainPage import MainPage
 from pages.ReviewBagPage import ReviewBagPage
-from pages.XIIIproPage import XIIIproPage
+from pages.XIIIProPage import XIIIProPage
 from tests import BaseTest
 
 
@@ -16,7 +16,7 @@ class Test(BaseTest):
         print("Apple Automation is Working For This Country:", country)
         main_page = MainPage(self.driver, lang_code=shortcode)
         iphone_page = IphonePage(self.driver)
-        pro_page = XIIIproPage(self.driver)
+        pro_page = XIIIProPage(self.driver)
         conf_page = ConfigurationPage(self.driver)
         rev_bag_page = ReviewBagPage(self.driver)
 
@@ -26,7 +26,7 @@ class Test(BaseTest):
 
         assert self.driver.title == assertion_values["iphone_title"]
         assert iphone_page.get_element_text(iphone_page.XIII_PRO) == assertion_values["menu_iphone_text"]
-        iphone_page.select_xii_pro()
+        iphone_page.select_xiii_pro()
 
         assert self.driver.title == assertion_values["pro_title"]
         assert pro_page.get_element_text(pro_page.BUY_BUTTON) == assertion_values["buy_button"]
@@ -37,7 +37,7 @@ class Test(BaseTest):
 
         conf_page.configure_additional_options(country)
 
-        conf_page.add_and_go_to_bag()
+        conf_page.go_to_bag()
 
         rev_bag_page.remove_iphone()
         assert assertion_values["page_source"] in self.driver.page_source
