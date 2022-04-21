cb36433478be9d64d48eacdc922360c37af18eae
Burak Bayramoğlu <bbayramoglu99@hotmail.com>
2022-04-21 17:06:44 +0300

    Add files via upload

--- pages/MainPage.py
+++ pages/MainPage.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
-
-from config.Config import TestData
 from pages.BasePage import BasePage
+from config.Config import TestData
 
 
 class MainPage(BasePage):
@@ -12,6 +11,15 @@ class MainPage(BasePage):
         self.driver = driver
         self.navigate_to_url(TestData.BASE_URL + lang_code)
 
+    def click_to_store(self):
+        raise NotImplemented
+
+    def click_to_mac(self):
+        raise NotImplemented
+
+    def click_to_ipad(self):
+        raise NotImplemented
+
     def click_to_iphone(self):
         self.highlight(self.IPHONE, "red")
-        self.click_visibility(self.IPHONE)
+        self.click(self.wait_for_visibility(self.IPHONE))
